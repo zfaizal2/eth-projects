afd1567bd2e680ecf527192e23ad940dad24a9b6
Zayyan Faizal <zfaizal2@illinois.edu>
2021-05-23 23:15:38 -0500

    new token contract

---
+++ defi/migrations/2_deploy_Tokens.js
@@ -0,0 +1,6 @@
+const MyToken = artifacts.require("MyToken")
+
+module.exports = async function (deployer, network, accounts) {
+    await deployer.deploy(MyToken)
+    const myToken = await MyToken.deployed()
+}
