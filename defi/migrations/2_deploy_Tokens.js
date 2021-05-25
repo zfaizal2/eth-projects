1d9d2a023fe4580d4ceac30fad1c655e113d7418
Zayyan Faizal <zfaizal2@illinois.edu>
2021-05-24 23:18:46 -0500

    FarmToken and tokenBalance implemented

--- defi/migrations/2_deploy_Tokens.js
+++ defi/migrations/2_deploy_Tokens.js
@@ -3,4 +3,7 @@ const MyToken = artifacts.require("MyToken")
 module.exports = async function (deployer, network, accounts) {
     await deployer.deploy(MyToken)
     const myToken = await MyToken.deployed()
+
+    await deployer.deploy(FarmToken, myToken.address)
+    const farmToken = await FarmToken.deployed()
 }
